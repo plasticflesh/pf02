7bd7251da891a30a83db0d60ce552ae7d6db1200
stieg <plasticflesh@gmail.com>
2015-01-03 09:32:01 -0800

    init push

---
+++ game.js
@@ -0,0 +1,11 @@
+function init() {
+	// create stage with id from html
+	var stage = new createjs.Stage("demoCanvas");
+	//
+	var circle = new createjs.Shape();
+	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+	circle.x = 100;
+	circle.y = 100;
+	stage.addChild(circle);
+	stage.update();
+}
